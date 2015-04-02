fd3ae7da89f9ae4ce20f5fa7169032b7939e8b2c
Forest Hu <foresthu2006@gmail.com>
2015-04-01 20:46:17 -0700

    Create SubsystemList.java

---
+++ src/org/usfirst/frc/team3473/robot/subsystems/SubsystemList.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team3473.robot.subsystems;
+
+/**
+ * @author Forest
+ */
+public class SubsystemList {
+	public static Drivetrain drivebase = new Drivetrain();
+	public static Elevator elevator = new Elevator();
+	public static IntakeArm intakeArm = new IntakeArm();
+}
