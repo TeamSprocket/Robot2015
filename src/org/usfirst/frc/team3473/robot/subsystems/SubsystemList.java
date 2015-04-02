07e62404cc1b92e1858315737c9180312eb1d10e
Forest Hu <foresthu2006@gmail.com>
2015-04-01 20:53:05 -0700

    Update SubsystemList.java

--- src/org/usfirst/frc/team3473/robot/subsystems/SubsystemList.java
+++ src/org/usfirst/frc/team3473/robot/subsystems/SubsystemList.java
@@ -6,5 +6,4 @@ package org.usfirst.frc.team3473.robot.subsystems;
 public class SubsystemList {
 	public static Drivetrain drivebase = new Drivetrain();
 	public static Elevator elevator = new Elevator();
-	public static IntakeArm intakeArm = new IntakeArm();
 }
